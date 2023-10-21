30a4d083d07d30a20334f15c6d0c1a4f0284aa38
topdev1030 <mercury427427@gmail.com>
2023-10-21 19:34:03 +0300

    Nuxt version upgraded

--- apps/next-app/next.config.js
+++
@@ -1,5 +0,0 @@
-const withTM = require("next-transpile-modules")(["react-ui"]);
-
-module.exports = withTM({
-  reactStrictMode: true,
-});
--- apps/next-app/pages/_app.tsx
+++
@@ -1,6 +0,0 @@
-import "../styles/global.css";
-
-function MyApp({ Component, pageProps }: any) {
-  return <Component {...pageProps} />;
-}
-export default MyApp;
